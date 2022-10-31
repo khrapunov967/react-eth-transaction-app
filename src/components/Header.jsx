b2b5a2c57bd7e27f01af536e6f6781f0a3990f91
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2022-10-31 15:23:10 +0700

    actually finalize ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,16 @@ const Header = () => {
     return (
         <header className="w-full flex justify-between items-center py-2 px-5 shadow-md mb-[80px]">
 
-            <div className="flex gap-3 items-center">
+            <div className="flex gap-3 items-center cursor-pointer">
                 <img src={LogoImage} alt="Logo Image" width={"47px"} height={"47px"}/>
                 <p className="text-white font-semibold text-2xl">Krypter.io</p>
             </div>
 
             <nav>
                 <ul className="flex items-center gap-7">
-                    <li><a href="#" className="text-white">Market</a></li>
-                    <li><a href="#" className="text-white">Exchange</a></li>
-                    <li><a href="#" className="text-white">Wallets</a></li>
+                    <li><a href="#" className="text-white transition-all duration-150 ease-linear hover:text-gray-200">Market</a></li>
+                    <li><a href="#" className="text-white transition-all duration-150 ease-linear hover:text-gray-200">Exchange</a></li>
+                    <li><a href="#" className="text-white transition-all duration-150 ease-linear hover:text-gray-200">Wallets</a></li>
                     <li><BlueRoundedButton title={"Login"}/></li>
                 </ul>
             </nav>
