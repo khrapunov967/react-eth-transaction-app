c560950abfa8370c3da22fdfe977111ab70d5a70
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2022-11-01 19:27:59 +0700

    fix design bugs

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -11,7 +11,7 @@ const MainSection = () => {
 
     return (
         <main className="w-full flex flex-row gap-5 px-[25px] mb-[80px] justify-between max-md-screen:flex-col max-md-screen:items-center ">
-            <section className="max-w-[450px] max-md-screen:mb-[20px]">
+            <section className="max-w-[450px] max-md-screen:mb-[20px] flex flex-col max-md-screen:items-center">
                 <h2 className="text-white text-[54px] font-bold leading-tight mb-5 max-md-screen:text-center max-sm-screen:text-[40px]">Send ETH cheap and rapidly.</h2>
                 
                 <p className="text-white opacity-50 mb-3 max-md-screen:hidden">Buy and sell cryptocurrencies, trusted by 10M wallets with over $30 billion in transactions.</p>
