a9905a1823e497a3c47f572e2571ca63911fe154
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2022-11-04 14:54:05 +0700

    fix side menu issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
             </div>
 
             <nav className="">
-                <ul className={`max-md-screen:animate-slideleft flex flex-row items-center gap-[35px] max-md-screen:justify-end max-md-screen:flex-col-reverse max-md-screen:items-start max-md-screen:absolute max-md-screen:top-0 max-md-screen:right-0 max-md-screen:bg-[#07071a] max-md-screen:p-3 max-md-screen:w-[200px] max-md-screen:gap-7 max-md-screen:h-screen max-md-screen:z-10 ${isMobileMenuVisible ? "" : "max-md-screen:hidden"}`}>
+                <ul className={`max-md-screen:animate-slideleft flex flex-row items-center gap-[35px] max-md-screen:justify-end max-md-screen:flex-col-reverse max-md-screen:items-start max-md-screen:fixed max-md-screen:top-0 max-md-screen:right-0 max-md-screen:bg-[#07071a] max-md-screen:p-3 max-md-screen:w-[200px] max-md-screen:gap-7 max-md-screen:h-screen max-md-screen:z-30 ${isMobileMenuVisible ? "" : "max-md-screen:hidden"}`}>
                     <li><a href="#" className="text-white transition-all duration-150 ease-linear hover:text-gray-200">Market</a></li>
                     <li><a href="#" className="text-white transition-all duration-150 ease-linear hover:text-gray-200">Exchange</a></li>
                     <li><a href="#" className="text-white transition-all duration-150 ease-linear hover:text-gray-200">Wallets</a></li>
